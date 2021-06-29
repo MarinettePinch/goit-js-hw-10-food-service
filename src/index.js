1867d4b71b89e0aa99f7b7a4182dc663557adba4
Marina Pinchuk <75565413+MarinettePinch@users.noreply.github.com>
2021-06-29 20:33:31 +0300

    Добавляет готовый проект

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import menuTemplate from './templates/menu.hbs';
+import menuData from './menu.json';
+
+const refs = {
+    body: document.querySelector('body'),
+    menu: document.querySelector('.js-menu'),
+    checkbox: document.querySelector('#theme-switch-toggle')
+}
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+const menuItems = menuData.map(menuTemplate).join(' ');
+
+
+
+refs.menu.insertAdjacentHTML('afterbegin', menuItems);
+
+refs.checkbox.addEventListener('change', onCheckboxChange);
+
+document.addEventListener('DOMContentLoaded', onLoad);
+
+function onCheckboxChange() {
+    if (refs.checkbox.checked) {
+        localStorage.setItem('theme', Theme.DARK);
+        setDarckTheme();
+    } else {
+        localStorage.setItem('theme', Theme.LIGHT);
+        setLightTheme();
+    }
+       
+}
+
+
+function onLoad() {
+    const theme = localStorage.getItem('theme');
+    if (theme === Theme.LIGHT) return;
+    refs.checkbox.checked = true;
+    setDarckTheme();
+    }
+
+function setDarckTheme() {
+        refs.body.classList.add(Theme.DARK);
+        refs.body.classList.remove(Theme.LIGHT);
+}
+
+function setLightTheme() {
+        refs.body.classList.add(Theme.LIGHT);
+        refs.body.classList.remove(Theme.DARK);
+}
